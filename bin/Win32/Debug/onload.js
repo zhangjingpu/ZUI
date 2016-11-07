b91fe812cbbd5f4fdb4b220be9ee6e3252f938cb
冉朋 <22249030@qq.com>
2016-11-07 14:31:03 +0800

    完善绑定及dome

--- bin/Win32/Debug/onload.js
+++ bin/Win32/Debug/onload.js
@@ -1,10 +1,29 @@
-﻿var a = GetByName("head");
-a.height=80;
-a.bkcolor=0xF00FFF00;
-a.drag=true;
-a.padding="10,5,20,6";
-a.ctl.bkcolor=0xF00FFFFF;
-print(a.height);
-function onclick(){
+﻿var root = GetByName("window");
+var tool = GetByName("tool");
+var BrowserTabHead = GetByName("BrowserTabHead");
+var BrowserTab = GetByName("BrowserTab");
+GetByName("ctl").WindowCtl_clos.onclick="clos_onclick();";
+tool.other.onclick="other_onclick();"
+
+AddBrowserTab("http://www.baidu.com");
+
+function AddBrowserTab(url){
+	var TabHead = new Control("Option");
+	TabHead.minwidth=60;
+	TabHead.maxwidth=130;
+	TabHead.group=true;
+	BrowserTabHead.AddAt(TabHead,BrowserTabHead.count-1);
+	TabHead.selected=true;
+
+	var Browser = new Control("Browser");
+	Browser.url=url;
+	BrowserTab.Add(Browser);
+	
+	BrowserTab.selectitem=BrowserTab.GetItemIndex(Browser);
+}
+function other_onclick(){
+	print("aa");
+}
+function clos_onclick(){
 	exit();
 }
