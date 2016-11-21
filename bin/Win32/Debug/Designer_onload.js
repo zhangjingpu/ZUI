a975dd8b597af7ef7e7447a4e414c838a1a43ee9
冉朋 <22249030@qq.com>
2016-11-21 18:28:34 +0800

    虚拟控件销毁失败处理

--- bin/Win32/Debug/Designer_onload.js
+++ bin/Win32/Debug/Designer_onload.js
@@ -1,37 +1,49 @@
 ﻿var root = GetByName("window");//取根对象
 var DesignTabHead = GetByName("designtabhead");//取设计区选择夹头部对象
 var DesignTab = GetByName("designtab");//取设计区选择夹对象
-var DesignTabSelectIndex=1;//当前选择的选择夹
+
 
 GetByName("ctl").WindowCtl_clos.onclick=function (c){exit();};//设置关闭按钮回调
 
-AddDesignTab();
-//AddDesignTab();
-
+//------------设计区
+var DesignTabSelectIndex=0;//当前选择的选择夹
 function DesignTabSelect(Index){
-	var old = DesignTabHead.GetItemAt(DesignTabSelectIndex);
-	if(old)
+	var old = DesignTabHead.GetItemAt(DesignTabSelectIndex-1);
+	if(old){
 		old.bkcolor=0;
+		old.closbut.visible=false;
+	}
 	var c = DesignTabHead.GetItemAt(Index-1);
-	c.bkcolor=0xFFFFF29D;
-	c.closbut.visible=true;
+	if(c){
+		c.bkcolor=0xFFFFF29D;
+		c.closbut.visible=true;
+	}
 	DesignTabSelectIndex=Index;
-	DesignTab.selectitem=Index;
+	DesignTab.selectitem=Index-1;
 }
 function AddDesignTab(){
 	var TabHead = new Control("HorizontalLayout");
-	//TabHead.bkcolor = 0xFFFFF29D;
 	TabHead.height=22;
 	TabHead.minwidth=60;
 	TabHead.maxwidth=170;
 	TabHead.onmouseenter=function(c){
-		
+		if(DesignTabHead.GetItemIndex(c)+1!=DesignTabSelectIndex){
+			c.bkcolor=0xFF5B7199;
+			c.closbut.visible=true;
+		}
 	};
 	TabHead.onmouseleave=function(c){
-		
-		
+		if(DesignTabHead.GetItemIndex(c)+1!=DesignTabSelectIndex){
+			c.bkcolor=0;
+			c.closbut.visible=false;
+		}	
 	};
-	TabHead.onclick=function(c){};
+	TabHead.onclick=function(c){
+		var Index=DesignTabHead.GetItemIndex(c)+1;
+		if(Index!=DesignTabSelectIndex){
+			DesignTabSelect(Index);
+		}
+	};
 	
 
 	var title = new Control("Label");
@@ -49,8 +61,10 @@ function AddDesignTab(){
 		var a=c.parent;
 		var i =DesignTabHead.GetItemIndex(a);
 		c.parent.clos();
-		DesignTab.GetItemAt(i+1).clos();
-		DesignTabSelect(i);
+		DesignTab.GetItemAt(i).clos();
+		if(i+1<=DesignTabSelectIndex)
+			DesignTabSelectIndex--;
+		DesignTabSelect(DesignTabSelectIndex);
 	}//关闭标签
 	clos.visible=false;
 	TabHead.Add(clos);
@@ -60,6 +74,25 @@ function AddDesignTab(){
 	
 	var Edit = new Control("CodeEdit");
 	DesignTab.Add(Edit);
-	DesignTabSelect(DesignTab.count-1);
+	DesignTabSelect(DesignTab.count);
 	return Edit;
 }
+
+
+//------------属性区
+
+
+
+//------------工具区
+
+
+
+//------------初始化代码
+AddDesignTab();
+AddDesignTab();
+AddDesignTab();
+AddDesignTab();
+
+
+
+
